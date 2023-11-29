68e7f19066d843fcbc05f85c1e1da7623200b528
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-11-29 11:49:18 +0100

    added

--- obj/Debug/net8.0/sampleDefaultAzureCredential.AssemblyInfo.cs
+++ obj/Debug/net8.0/sampleDefaultAzureCredential.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sampleDefaultAzureCredential")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18c4ecdbc9adffe51eb5e2b102aa750c538418e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("sampleDefaultAzureCredential")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sampleDefaultAzureCredential")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
